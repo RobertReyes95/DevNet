7c2cb261090a9b627c78d01835e1900c9d5b47db
Robert Reyes <Robert.Reyes@elma.com>
2021-02-23 15:44:25 -0800

    Adding sample.js

---
+++ sample.js
@@ -0,0 +1,22 @@
+myObj = [ //This is a javascript object
+    { 
+        "Id": "1", 
+        "FirstName":"Rob",
+        "LastName":"Rey", 
+        "Email":"robert.reyes@elma.com" 
+    }, 
+    {
+        "Id": "2", 
+        "FirstName":"Jonas",
+        "LastName":"Smith", 
+        "Email":"jonas.smith@elma.com" 
+    },
+    {
+        "Id": "3", 
+        "FirstName":"Gregg",
+        "LastName":"Bishop", 
+        "Email":"gregg.bishop@elma.com" 
+    }
+]
+
+console.log(myObj[0].FirstName)
