bec90aa410729290bbb57a6988707dcf0de0977f
Robert Reyes <Robert.Reyes@elma.com>
2021-02-25 15:01:01 -0800

    Addding sample2.py

---
+++ sample2.py
@@ -0,0 +1,10 @@
+import xmltodict
+
+#Get the XML file data from
+stream = open ('sample.xml', 'r')
+
+#Prase  the XML file into an 'OrderDict'
+xml = xmltodict.parse(stream.read())
+
+for e in xml["People"]:
+    print(e)
